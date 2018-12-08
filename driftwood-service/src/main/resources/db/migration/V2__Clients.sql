400c31e256cb06d189b7e1d0769d30b433e7f5f6
Graham Cox <graham@grahamcox.co.uk>
2018-12-08 19:20:05 +0000

    Database definition of an OpenID Connect Client

---
+++ driftwood-service/src/main/resources/db/migration/V2__Clients.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clients(
+                      client_id UUID NOT NULL PRIMARY KEY,
+                      version UUID NOT NULL,
+                      created TIMESTAMP NOT NULL,
+                      updated TIMESTAMP NOT NULL,
+                      name TEXT NOT NULL,
+                      owner_id UUID NOT NULL REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                      redirect_uris TEXT[] NOT NULL,
+                      response_types TEXT[] NOT NULL,
+                      grant_types TEXT[] NOT NULL
+);
