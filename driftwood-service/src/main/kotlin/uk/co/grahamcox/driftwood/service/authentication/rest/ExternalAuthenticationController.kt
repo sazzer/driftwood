250f2a33cdaf7ce9452826caec5ffd127d3aeed6
Graham Cox <graham@grahamcox.co.uk>
2019-01-05 17:31:31 +0000

    When starting authentication, return a 302 to redirect the user, and include some cookies to store the authentication state

--- driftwood-service/src/main/kotlin/uk/co/grahamcox/driftwood/service/authentication/rest/ExternalAuthenticationController.kt
+++ driftwood-service/src/main/kotlin/uk/co/grahamcox/driftwood/service/authentication/rest/ExternalAuthenticationController.kt
@@ -1,10 +1,14 @@
 package uk.co.grahamcox.driftwood.service.authentication.rest
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import uk.co.grahamcox.driftwood.service.authentication.AuthenticatorRegistry
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
 
 /**
  * Controller for managing External Authentication requests
@@ -25,5 +29,14 @@ class ExternalAuthenticationController(private val authenticatorRegistry: Authen
      * @return the details to start authentication
      */
     @RequestMapping(value = ["/{service}/start"], method = [RequestMethod.GET])
-    fun startAuthentication(@PathVariable("service") service: String) = authenticatorRegistry[service].startAuthentication()
+    fun startAuthentication(@PathVariable("service") service: String, response: HttpServletResponse): ResponseEntity<Void> {
+        val result = authenticatorRegistry[service].startAuthentication()
+
+        response.addCookie(Cookie("externalAuthenticationService", service))
+        response.addCookie(Cookie("externalAuthenticationState", result.state))
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(result.redirect)
+                .build<Void>()
+    }
 }
