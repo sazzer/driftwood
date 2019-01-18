d2a0ad002f1b6a2f6123e220e42eed83309b01da
Graham Cox <graham@grahamcox.co.uk>
2019-01-18 09:43:35 +0000

    Stripped out Redux Box since it doesn't work with the latest Redux

--- driftwood-ui/src/redux/index.js
+++ driftwood-ui/src/redux/index.js
@@ -1,20 +1,24 @@
 // @flow
 
-import {createStore} from 'redux-box';
 import {connectRouter, routerMiddleware} from 'connected-react-router';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import {createBrowserHistory} from 'history';
-import {module as dummyModule} from './dummyModule';
 
+/** The browser history */
 export const history = createBrowserHistory();
 
-const reduxBoxConfig = {
-    middlewares: [
-        routerMiddleware(history)
-    ],
-    reducers: {
-        router: connectRouter(history)
-    }
-};
-export const store = createStore([
-    dummyModule
-], reduxBoxConfig);
+/** The reducer to use for the application */
+const reducer = combineReducers({
+    router: connectRouter(history)
+});
+
+/** The Redux store to use */
+export const store = createStore(
+    reducer,
+    {},
+    compose(
+        applyMiddleware(
+            routerMiddleware(history),
+        ),
+    ),
+);
