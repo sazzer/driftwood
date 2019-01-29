f5bfd012c09f3c2a7e2d778c90bd080075688ad0
Graham Cox <graham@grahamcox.co.uk>
2019-01-29 08:13:27 +0000

    Make the Edit Profile option an actual link, even if it goes nowhere yet

--- driftwood-ui/src/ui/header/ProfileMenu/ProfileMenu.js
+++ driftwood-ui/src/ui/header/ProfileMenu/ProfileMenu.js
@@ -4,6 +4,12 @@ import React from 'react';
 import {Dropdown} from 'semantic-ui-react';
 import {NamespacesConsumer} from "react-i18next";
 import UsernameConsumer from "../../../users/UsernameConsumer";
+import {Link} from "react-router-dom";
+
+/** Styles to apply to the Profile Link */
+const ProfileLinkStyles = {
+    color: 'black',
+};
 
 /**
  * The props for the Profile Menu component
@@ -24,7 +30,12 @@ export default function ProfileMenu({userId}: ProfileMenuProps) {
                     <NamespacesConsumer>
                         {(t) =>
                             <Dropdown.Menu>
-                                <Dropdown.Item data-test='profile' text={t('profile.menu.editProfile')} />
+                                <Dropdown.Item data-test='profile'>
+                                    <Link to='/profile' style={ProfileLinkStyles}>
+                                        {t('profile.menu.editProfile')}
+                                    </Link>
+                                </Dropdown.Item>
+
                                 <Dropdown.Divider/>
                                 <Dropdown.Item data-test='logout' text={t('profile.menu.logout')} />
                             </Dropdown.Menu>
