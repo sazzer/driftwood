a174e0406fd244456c44f1d906af615aebc8af53
Graham Cox <graham@grahamcox.co.uk>
2019-01-31 07:33:37 +0000

    Write out screenshots and logs more reliably

--- driftwood-e2e/src/browser/hooks.js
+++ driftwood-e2e/src/browser/hooks.js
@@ -22,11 +22,23 @@ Before(function() {
 });
 
 After(async function(testCase) {
-    const fileBase = 'screenshots/out';
+    fs.mkdirSync('screenshots', {recursive: true});
 
+    const featureFile = testCase.sourceLocation.uri;
+    const scenario = testCase.pickle.name;
+
+    const fileFeatureBase = featureFile.replace(/^features\//, '')
+        .replace(/\.feature$/, '')
+        .replace(/[^A-Za-z0-9_-]/g, '_');
+    const fileScenarioBase = scenario.replace(/[^A-Za-z0-9_-]/g, '_');
+
+    const fileBase = `screenshots/${fileFeatureBase}_${fileScenarioBase}`;
+
+    console.log('Writing screenshot to file: %s', fileBase + '.png');
     const image = await driver.takeScreenshot();
     fs.writeFileSync(`${fileBase}.png`, image, 'base64');
 
+    console.log('Writing browser log to file: %s', fileBase + '.log');
     const logs = await driver.manage().logs().get(logging.Type.BROWSER);
     const browserLog = logs.map(entry => `[${entry.level.name}] ${entry.message}`)
         .join('\n');
