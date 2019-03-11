c67bd30f323b47bbfe3600d99cbf048600b01ebe
Graham Cox <graham@grahamcox.co.uk>
2019-03-11 13:10:26 +0000

    Support for environment configuration

--- driftwood-go/cmd/driftwood/driftwood.go
+++ driftwood-go/cmd/driftwood/driftwood.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"context"
+	"github.com/heetch/confita"
 	log "github.com/sirupsen/logrus"
 	"os"
 )
 
+// Config represents the environment configuration for the application
+type Config struct {
+	// Debug represents whether we are executing in Debug mode or not
+	Debug bool `config:"debug"`
+}
+
 func main() {
+	cfg := Config{
+		Debug: false,
+	}
+
+	err := confita.NewLoader().Load(context.Background(), &cfg)
+	if err != nil {
+		panic(err)
+	}
+
 	log.SetOutput(os.Stdout)
-	log.SetLevel(log.DebugLevel)
+	if cfg.Debug {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
 
 	log.WithFields(log.Fields{
 		"animal": "walrus",
 		"size":   10,
-	}).Info("A group of walrus emerges from the ocean")
+	}).Debug("A group of walrus emerges from the ocean")
 
 }
--- driftwood-go/go.mod
+++ driftwood-go/go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/heetch/confita v0.5.1
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/mattn/go-isatty v0.0.6 // indirect
@@ -15,7 +16,7 @@ require (
 	github.com/sirupsen/logrus v1.3.0
 	github.com/stretchr/testify v1.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
-	golang.org/x/net v0.0.0-20190310074541-c10a0554eabf // indirect
+	golang.org/x/net v0.0.0-20190311031020-56fb01167e7d // indirect
 	golang.org/x/sys v0.0.0-20190310054646-10058d7d4faa // indirect
 	golang.org/x/tools v0.0.0-20190308174544-00c44ba9c14f // indirect
 )
